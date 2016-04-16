4de3e0eea457068de6d283b6e93e0462dc6cf276
khoonseng <khoonseng@khoonsengs-mac.local>
2016-04-16 23:47:38 +0800

    Initial Commit

---
+++ ks-realtimeappTests/ks_realtimeappTests.swift
@@ -0,0 +1,36 @@
+//
+//  ks_realtimeappTests.swift
+//  ks-realtimeappTests
+//
+//  Created by khoonseng on 4/16/16.
+//  Copyright © 2016 khoonseng. All rights reserved.
+//
+
+import XCTest
+@testable import ks_realtimeapp
+
+class ks_realtimeappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
