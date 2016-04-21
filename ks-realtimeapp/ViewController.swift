7a18557b94c1f0a7ee39b21bf4e9c40664f7499f
khoonseng <khoonseng@khoonsengs-mac.local>
2016-04-21 21:10:36 +0800

    add new label

--- ks-realtimeapp/ViewController.swift
+++ ks-realtimeapp/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textLabel: UILabel!
+    let firebase = Firebase (url: "https://ks-realtimeapp.firebaseio.com")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        firebase.setValue("App started")
+        
+        firebase.observeEventType(FEventType.Value) { (snapshot:FDataSnapshot!) -> Void in
+            //print(snapshot.value)
+            self.textLabel.text = snapshot.value as? String
+            
+            //self.firebase.setValue("Computer says no!")
+        }
     }
 
     override func didReceiveMemoryWarning() {
