5326fdd6470ebb0b439d21b5025af8ea1bc8da90
khoonseng <khoonseng@khoonsengs-mac.local>
2016-04-23 12:05:02 +0800

    add new segue to display new modal to add message. created new MessageViewController

---
+++ ks-realtimeapp/MessageViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MessageViewController.swift
+//  ks-realtimeapp
+//
+//  Created by khoonseng on 4/23/16.
+//  Copyright © 2016 khoonseng. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+
+    @IBAction func sendMessage(sender: AnyObject) {
+    }
+    @IBAction func cancelMessage(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
