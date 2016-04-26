9082f2661e06a20e54f64c2b15002ad0a0d65ce9
khoonseng <khoonseng@khoonsengs-mac.local>
2016-04-27 00:44:36 +0800

    added JSQMessagesViewController attributes

--- ks-realtimeapp/JSQViewController.swift
+++ ks-realtimeapp/JSQViewController.swift
@@ -14,22 +14,14 @@ class JSQViewController: JSQMessagesViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.senderId = "uidFromFirebase"
+        self.senderDisplayName = "username"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
