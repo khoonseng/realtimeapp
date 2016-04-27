a4105f13c8c0bf0561ef91a5c3b18cc82d41df1a
khoonseng <khoonseng@khoonsengs-mac.local>
2016-04-27 20:50:32 +0800

    Create avatars and message bubbles

--- ks-realtimeapp/JSQViewController.swift
+++ ks-realtimeapp/JSQViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class JSQViewController: JSQMessagesViewController {
 
+    var incomingBubble: JSQMessagesBubbleImage!
+    var outgoingBubble: JSQMessagesBubbleImage!
+    var avatars = [String: JSQMessagesAvatarImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,11 +21,26 @@ class JSQViewController: JSQMessagesViewController {
         
         self.senderId = "uidFromFirebase"
         self.senderDisplayName = "username"
+        self.inputToolbar.contentView.leftBarButtonItem.hidden = true
+        self.inputToolbar.contentView.leftBarButtonItemWidth = 0
         
+        let bubbleFactory = JSQMessagesBubbleImageFactory()
+        incomingBubble = bubbleFactory.incomingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleBlueColor())
+        outgoingBubble = bubbleFactory.outgoingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleLightGrayColor())
+        
+        createAvatar(senderId, senderDisplayName: senderDisplayName, color: UIColor.lightGrayColor())
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func createAvatar(senderID:String,senderDisplayName:String, color:UIColor) {
+        if avatars[senderID] == nil {
+            let initials = senderDisplayName.substringToIndex(senderDisplayName.startIndex.advancedBy(min(2,senderDisplayName.characters.count)))
+            let avatar = JSQMessagesAvatarImageFactory.avatarImageWithUserInitials(initials, backgroundColor: color, textColor: UIColor.blackColor(), font: UIFont.systemFontOfSize(14), diameter: UInt(kJSQMessagesCollectionViewAvatarSizeDefault))
+            avatars[senderID] = avatar
+        }
     }
 }
